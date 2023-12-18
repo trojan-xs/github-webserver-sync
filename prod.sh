d08798d9d5a356e486a8c7d5ddb6d9d430046fff
trojan-xs <49181542+trojan-xs@users.noreply.github.com>
2023-12-17 21:02:50 -0800

    Update prod.sh
    Script does not ask for authorization before deleting files from directory.
    
    Checks for directory, If the directory does not exist, it creates the directory before dumping all of the repo files in the directory.
    
    If the directory exists, delete all files and subdirectories and replaces it with files from repo.

--- prod.sh
+++ prod.sh
@@ -1,48 +1,29 @@
 #!/bin/bash
 
-# Function to check if a directory is empty
-is_directory_empty() {
-    shopt -s nullglob
-    files=("$1"/*)
-    shopt -u nullglob
-    if [ ${#files[@]} -eq 0 ]; then
-        return 0   # Directory is empty
-    else
-        return 1   # Directory is not empty
-    fi
-}
-
-# Function to clone GitHub repo into a directory
-clone_repo() {
-    repo_url=$1
-    target_dir=$2
-
-    # Check if the target directory exists
-    if [ ! -d "$target_dir" ]; then
-        # If it doesn't exist, create and clone into it
-        mkdir "$target_dir"
-        git clone "$repo_url" "$target_dir"
-    else
-        # If it exists, check if it's empty
-        if is_directory_empty "$target_dir"; then
-            # If it's empty, rename and create a new directory
-            timestamp=$(date +"%Y-%m-%d-%H-%M-%S")
-            mv "$target_dir" "$target_dir-$timestamp.old"
-            mkdir "$target_dir"
-            git clone "$repo_url" "$target_dir"
-        else
-            # If it's not empty, just clone into it
-            git clone "$repo_url" "$target_dir"
-        fi
-    fi
-}
-
-# Check if the correct number of arguments is provided
+# Check if correct number of arguments are provided
 if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <repo_url> <target_directory>"
+    echo "Usage: $0 <GitHub Repo Link> <Global Path to Directory>"
     exit 1
 fi
 
-# Call the function with provided arguments
-clone_repo "$1" "$2"
+# Assign arguments to variables
+github_repo="$1"
+directory_path="$2"
+
+# Check if the directory exists
+if [ -d "$directory_path" ]; then
+    # Directory exists, delete its contents
+    echo "Directory exists. Deleting contents..."
+    find "$directory_path" -mindepth 1 -delete
+else
+    # Directory does not exist, create it
+    echo "Creating directory..."
+    mkdir -p "$directory_path"
+fi
+
+# Clone the GitHub repo into the directory
+echo "Cloning GitHub repo into the directory..."
+git clone "$github_repo" "$directory_path"
+
+echo "Process completed successfully."
 
