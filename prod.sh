f2269706ff4352023e709265e7bfc7df14dcfd1b
trojan-xs <49181542+trojan-xs@users.noreply.github.com>
2023-12-17 21:00:31 -0800

    Update prod.sh

--- prod.sh
+++ prod.sh
@@ -1,63 +1,48 @@
 #!/bin/bash
 
-# Function to display usage information
-usage() {
-    echo "Usage: $0 <github_repository> <directory_name>"
-    exit 1
+# Function to check if a directory is empty
+is_directory_empty() {
+    shopt -s nullglob
+    files=("$1"/*)
+    shopt -u nullglob
+    if [ ${#files[@]} -eq 0 ]; then
+        return 0   # Directory is empty
+    else
+        return 1   # Directory is not empty
+    fi
+}
+
+# Function to clone GitHub repo into a directory
+clone_repo() {
+    repo_url=$1
+    target_dir=$2
+
+    # Check if the target directory exists
+    if [ ! -d "$target_dir" ]; then
+        # If it doesn't exist, create and clone into it
+        mkdir "$target_dir"
+        git clone "$repo_url" "$target_dir"
+    else
+        # If it exists, check if it's empty
+        if is_directory_empty "$target_dir"; then
+            # If it's empty, rename and create a new directory
+            timestamp=$(date +"%Y-%m-%d-%H-%M-%S")
+            mv "$target_dir" "$target_dir-$timestamp.old"
+            mkdir "$target_dir"
+            git clone "$repo_url" "$target_dir"
+        else
+            # If it's not empty, just clone into it
+            git clone "$repo_url" "$target_dir"
+        fi
+    fi
 }
 
 # Check if the correct number of arguments is provided
 if [ "$#" -ne 2 ]; then
-    usage
+    echo "Usage: $0 <repo_url> <target_directory>"
+    exit 1
 fi
 
-# Assign input parameters to variables
-github_repository="$1"
-directory_name="$2"
-
-# Check if the original directory exists
-if [ -d "$directory_name" ]; then
-    # Check if the directory is empty
-    if [ -z "$(ls -A "$directory_name")" ]; then
-        # If the directory is empty, rename the original directory with a timestamp
-        current_date=$(date +"%y-%m-%d-%H-%M-%S")
-        mv "$directory_name" "${directory_name}-${current_date}.old"
-        echo "Renamed existing empty directory to ${directory_name}-${current_date}.old"
-
-        # Create a new directory with the specified name
-        mkdir "$directory_name"
-        echo "Created new directory: $directory_name"
-
-        # Clone the GitHub repository into the specified directory
-        git clone "$github_repository" "$directory_name"
-        
-        # Check if the cloning was successful
-        if [ $? -eq 0 ]; then
-            echo "Repository cloned successfully into $directory_name"
-        else
-            echo "Failed to clone the repository. Please check the GitHub repository URL and try again."
-        fi
-    else
-        # If the directory is not empty, clone the GitHub repository into it
-        git clone "$github_repository" "$directory_name"
-        
-        # Check if the cloning was successful
-        if [ $? -eq 0 ]; then
-            echo "Repository cloned successfully into $directory_name"
-        else
-            echo "Failed to clone the repository. Please check the GitHub repository URL and try again."
-        fi
-    fi
-else
-    # If the original directory does not exist, create it and clone the GitHub repository into it
-    echo "Directory $directory_name does not exist. Creating and cloning into it..."
-    git clone "$github_repository" "$directory_name"
-    
-    # Check if the cloning was successful
-    if [ $? -eq 0 ]; then
-        echo "Repository cloned successfully into $directory_name"
-    else
-        echo "Failed to clone the repository. Please check the GitHub repository URL and try again."
-    fi
-fi
+# Call the function with provided arguments
+clone_repo "$1" "$2"
 
